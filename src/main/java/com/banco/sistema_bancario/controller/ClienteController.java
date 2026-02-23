d3d1211a5c74784a6e25d0e923456a35e8da5b69
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-22 21:23:37 -0300

    Inicialização do controller!

--- src/main/java/com/banco/sistema_bancario/controller/ClienteController.java
+++ src/main/java/com/banco/sistema_bancario/controller/ClienteController.java
@@ -1,4 +1,24 @@
 package com.banco.sistema_bancario.controller;
 
+import com.banco.sistema_bancario.dto.cliente.*;
+import com.banco.sistema_bancario.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/clientes")
+@RequiredArgsConstructor
 public class ClienteController {
+
+    private final ClienteService service;
+
+    @PostMapping
+    public ResponseEntity<ClienteResponseDTO> criar(
+            @RequestBody @Valid ClienteRequestDTO dto) {
+
+        return ResponseEntity.status(201).body(service.criar(dto));
+    }
 }
