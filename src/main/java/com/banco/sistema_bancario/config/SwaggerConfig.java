6491e790293d3879798a898ecb6f5ea8d476cb6a
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-22 21:29:46 -0300

    Finalização do Cors

--- src/main/java/com/banco/sistema_bancario/config/SwaggerConfig.java
+++ src/main/java/com/banco/sistema_bancario/config/SwaggerConfig.java
@@ -1,4 +1,31 @@
 package com.banco.sistema_bancario.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Banco Digital")
+                        .description("Documentação da API do sistema bancário")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Vini")
+                                .email("vini@email.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentação Completa")
+                        .url("https://springdoc.org"));
+    }
 }
